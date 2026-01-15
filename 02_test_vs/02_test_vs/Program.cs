f9611b0704e4b365e15055b71cd00ae5a9cd6ee9
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-01-15 23:56:51 +0300

    add: intro

---
+++ 02_test_vs/02_test_vs/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _02_test_vs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ 04_base/01_var_types/Program.cs
@@ -0,0 +1,115 @@
+﻿
+#region Intro
+// Console.WriteLine("Test string");
+// Console.WriteLine("Vasia");
+
+/*
+lkj
+lkj
+lkj 
+ */
+
+//Console.WriteLine("Hello Vasia");
+//Console.Write("Hello Petya");
+
+//string input = Console.ReadLine();
+
+//Console.BackgroundColor = ConsoleColor.Red;
+//Console.ForegroundColor = ConsoleColor.Blue;
+//Console.WriteLine("WITH COLOR");
+//Console.ResetColor();
+
+//Console.SetCursorPosition(20, 5);
+//Console.WriteLine("WITH SEEK");
+#endregion
+
+
+#region vars
+
+//int a;
+
+//int _b;
+
+////int 3c;     // ERROR
+
+//int d = 3;
+
+//int e;
+//e = 12;
+
+//Console.WriteLine(e);
+
+
+//const int f = 45;
+//// f = 12;                 // ERROR
+
+
+#endregion
+
+
+#region Types
+
+// ================ Значимые типы =================
+
+// СТРУКТУРЫ
+
+// bool
+bool a = true;
+
+//byte (0 - 255)
+byte b = 255;
+
+// sbyte (-128 -- 127)
+sbyte c = -45;
+
+// short (-32768 -- 32767)
+short d = -1000;
+
+// ushort (0 -- 65535)
+ushort e = 12;
+
+// int
+int f = 23;
+
+// uint
+uint g = 45;
+
+// long
+long h = -123;
+
+// ulong
+ulong i = 123;
+
+// float (2b)
+float j = 4.56f;
+
+// double
+double k = 4.45;
+
+// decimal
+decimal l = 1.234234234234234234m;
+
+// char
+char m = 'x';           // UNICODE
+char n = '⦻';
+char o = '\u0E18';
+
+
+// ================ Ссылочные типы ================
+
+// КЛАССЫ
+
+string s = "Vasia";
+
+object q = 34;
+
+
+
+//string input = "12";
+//int val = Int32.Parse(input);
+
+
+#endregion
+
+
+
