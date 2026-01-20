85d243381fae0f9b3af4a42ad21ff8effcc347a7
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-01-20 19:45:51 +0300

    add: operators

--- 04_base/01_var_types/Program.cs
+++ 04_base/01_var_types/Program.cs
@@ -51,65 +51,107 @@ lkj
 
 // ================ Значимые типы =================
 
-// СТРУКТУРЫ
+//// СТРУКТУРЫ
 
-// bool
-bool a = true;
+//// bool
+//bool a = true;
 
-//byte (0 - 255)
-byte b = 255;
+////byte (0 - 255)
+//byte b = 255;
 
-// sbyte (-128 -- 127)
-sbyte c = -45;
+//// sbyte (-128 -- 127)
+//sbyte c = -45;
 
-// short (-32768 -- 32767)
-short d = -1000;
+//// short (-32768 -- 32767)
+//short d = -1000;
 
-// ushort (0 -- 65535)
-ushort e = 12;
+//// ushort (0 -- 65535)
+//ushort e = 12;
 
-// int
-int f = 23;
+//// int
+//int f = 23;
 
-// uint
-uint g = 45;
+//// uint
+//uint g = 45;
 
-// long
-long h = -123;
+//// long
+//long h = -123;
 
-// ulong
-ulong i = 123;
+//// ulong
+//ulong i = 123;
 
-// float (2b)
-float j = 4.56f;
+//// float (2b)
+//float j = 4.56f;
 
-// double
-double k = 4.45;
+//// double
+//double k = 4.45;
 
-// decimal
-decimal l = 1.234234234234234234m;
+//// decimal
+//decimal l = 1.234234234234234234m;
 
-// char
-char m = 'x';           // UNICODE
-char n = '⦻';
-char o = '\u0E18';
-
-
-// ================ Ссылочные типы ================
-
-// КЛАССЫ
-
-string s = "Vasia";
-
-object q = 34;
+//// char
+//char m = 'x';           // UNICODE
+//char n = '⦻';
+//char o = '\u0E18';
 
 
+//// ================ Ссылочные типы ================
 
-//string input = "12";
-//int val = Int32.Parse(input);
+//// КЛАССЫ
+
+//string s = "Vasia";
+
+//object q = 34;
+
+
+
+////string input = "12";
+////int val = Int32.Parse(input);
 
 
 #endregion
 
 
+#region Literal
 
+//bool a = true;
+
+//int b = 5;
+//int c = 0b011010;
+//int d = 0xFF;
+
+//double e = 4.5;
+//double f = -0.000001;
+//double g = 2.1e3;           // 2.1 * 10^3
+//double h = 3.2e-28;         // 3.2 * 10^-28
+
+//char j = 'x';
+////char k = 45;                // ERROR
+//char l = '\n';                // также \t\b\0...
+//char m = '\x90';
+
+//string n = "Hello Vasia";
+//string o = "Hello \"Vasia\"";
+//// Console.WriteLine(o);
+//string p = @"https://wikipedia.org";
+//string q = @"SELECT *
+//        FROM users;";
+//Console.WriteLine(q);
+
+//// string interpolation
+//string email = "vasia@mail.com";
+//int age = 23;
+//string r = $"email: {email}, age: {age}";
+//Console.WriteLine(r);
+
+#endregion
+
+
+// C
+// write(1, "Vasia", 5)
+
+// C++
+// std::cout << "Vasia";
+
+// C#
+// Console.Write("Vasia");
---
+++ 04_base/02_operators/Program.cs
@@ -0,0 +1,94 @@
+﻿
+#region Base operators
+
+// +  -  *  /  %
+
+// ++  --
+
+// &    |    ^    ~    <<    >>
+
+// =    +=  -=  *=  /=  %=      &=  |=  ^=  ~=  <<=  >>=
+
+//byte a = 255;               // 11111111
+//byte b = (byte)(a + 1);     //100000000
+
+// ==   !=    <     >       <=      >=
+
+// &&(сокращённое)  &(полное)  ||(сокращённое)  |(полное)       !
+
+//int a = 4;
+//int b = 5;
+
+//bool res1 = (a > b) && (a < b);
+//bool res2 = (a > b) & (a < b);
+
+//bool res3 = (a > b) || (a < b);
+//bool res4 = (a > b) | (a < b);
+
+
+#endregion
+
+#region Operators
+
+
+//if (<condition>)
+//{
+
+//}
+//else if (<condition 2>)
+//{
+
+//}
+//else
+//{
+
+//}
+
+
+//int a = 3;
+//int b = a > 0 ? 1 : 0;
+
+#endregion
+
+#region Cycles
+
+// for  do/while    while   foreach
+//for (int i = 0; i < 100; ++i)
+//{
+//    // break;
+//    // continue;
+//}
+
+//for(; ; )
+//{
+
+//}
+
+//int i = 10;
+//do
+//{
+//    //
+//    //
+//    --i;
+//} while (i > 0);
+
+//int j = 10;
+//while(j > 0)
+//{
+//    //
+//    //
+//    --j;
+//}
+
+
+//foreach(variable in enumerable)
+//{
+
+//}
+
+string email = "vasia@mail.com";
+foreach(char c in email)
+    Console.WriteLine(c);
+
+
+#endregion
