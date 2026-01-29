8e19a98eeba4ff1e047d2550caf64b829ec2217e
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-01-30 01:23:44 +0300

    add:git

---
+++ 05_oop/01_class/Program.cs
@@ -0,0 +1,28 @@
+﻿
+#region Intro
+
+User user = new User();
+user.id = 101;
+user.email = "vasia@mail.com";
+
+user.Render();
+
+
+
+
+class User
+{
+    public int id;
+    public string email;
+
+    public void Render()
+    {
+        Console.WriteLine($"User info: {id} --> {email}");
+    }
+}
+
+
+
+#endregion
+
+
