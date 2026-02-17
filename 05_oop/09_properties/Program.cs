92bdf9f3b008f6ebbffa6fde97a2f948ac67394d
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-02-17 23:11:18 +0300

    add: properties, static, const

--- 05_oop/09_properties/Program.cs
+++ 05_oop/09_properties/Program.cs
@@ -126,15 +126,168 @@
 
 
 #region AutoProperties
-Console.WriteLine();
-class User
-{
-    public int Id { get; set; }     // auto prop
-    public int Age { get; } = 18;
-	
 
+//Console.WriteLine();
+//class User
+//{
+//    public int Id { get; set; }     // auto prop
+//    public int Age { get; } = 18;
+//    public string Name { get; private set; }
+//    public string Password { get; set; } = "no_password";
 
-}
+//    public User(int age)
+//    {
+//        Age = age;
+//    }
+//    public void Execute()
+//    {
+//        // Age = 23;               // ERROR
+//    }
 
+//}
 
 #endregion
+
+
+#region init block
+
+//User user1 = new();
+//User user2 = new("vasia@mail.com");
+//User user3 = new() { Email = "petya@mail.com" };
+//User user4 = new() { Username = "chack_norris" };
+
+//class User
+//{
+//    private string username;
+//    public string Email { get; set; } = "no_email";
+//    public string Username
+//    {
+//        get
+//        {
+//            return username;
+//        }
+//        init        // Можно использовать в: определение дефолтного значения, конструкторе, инициализаторе
+//        {
+//            username = value;
+//            Email = $"{username}@mail.com";
+//        }
+//    }
+
+//    public string Role { get; init; } = "guest";
+
+//    public string Password { get; } = "no_password";
+
+//    public User()
+//    { }
+//    public User(string email)
+//    {
+//        Username = "const_default_username";
+//    }
+//}
+
+#endregion
+
+
+#region Practice
+
+//// User a = new() { Id = 303 };    // ERROR
+//// User b = new() { Email = "no_email" };
+//User c = new() { Password = "password" };
+//// User d = new() { Level = 14 };   // ERROR
+//class User
+//{
+//    private int Id { get; } = 101;
+//    public string Email { get; private set; } = "vasia@mail.com";
+//    public string Password { private get; init; } = "123123123";
+//    public int Level { get; private init; } = 10;
+//    public User()
+//    {
+//        Id = 202;
+//        Email = "petya@mail.com";
+//        Password = "56756567567";
+//        Level = 12;
+//    }
+//}
+
+#endregion
+
+
+#region BUNS :-)
+
+//class User
+//{
+//    private string email;
+//    private string password;
+
+//    public string Email
+//    {
+//        get => email;
+//        set => email = value;
+//    }
+
+//    //public string Password
+//    //{
+//    //    get => password;
+//    //}
+//    // >>> EQUALS <<<
+//    public string Password => password;
+//}
+
+#endregion
+
+
+#region required
+
+//User a = new User();                    // ERROR
+
+//User b = new User("vasia@mail.com");    // ERROR
+
+//User c = new User("petya@mail.com")     // ERROR
+//{
+//    Age = 23,
+//};
+
+//User d = new User("petya@mail.com")
+//{
+//    Age = 23,
+//    Email = "chack@mail.com",
+//};
+
+//class User
+//{
+//    public required int Age { get; set; }
+//    public required string Email { get; set; }
+//    public User()
+//    { }
+//    public User(string email)
+//    {
+//        Email = email;
+//    }
+//}
+
+#endregion
+
+
+#region field   FROM C#14
+
+//class User
+//{
+//    private int id;
+//    public int Id 
+//    { 
+//        get => id; 
+//        set => id = value < 0 ? 0 : value ;
+//    }
+
+
+
+//    public string Email
+//    {
+//        get => field;
+//        set => field = value;
+//    }
+//}
+
+#endregion
+
+
---
+++ 05_oop/10_overloading_static_const/Program.cs
@@ -0,0 +1,81 @@
+﻿
+
+#region Overloading
+// 1. Имя должно совпадать
+// 2. Другое кол-во параметров
+// 3. Тип или порядок параметров
+// 4. Модификаторы параметров
+
+//class Container
+//{
+//    public void Method(int a, int b)
+//    { }
+
+//    public void Method(int a, int b, int c)
+//    { }
+
+//    public void Method(int a, string b)
+//    { }
+
+//    public void Method(ref int a, ref readonly int b)
+//    { }
+//}
+
+#endregion
+
+
+#region Static
+
+User a = new User();
+User b = new User();
+
+Console.WriteLine($"{a.id} {b.id}");
+Console.WriteLine($"MaxId = {User.MaxId}");
+Console.WriteLine($"Count = {User.InstanceCount}");
+
+class User
+{
+    // Статическое поле (НЕ является частью состояния объекта)
+    private static int maxId;
+
+    // обычное поле (является частью состояния объекта)
+    public int id;
+
+    // Статический конструктор (инициализирует статические поля и св-ва)
+    // 1. Нельзя использовать модификатор доступа
+    // 2. Нет this
+    // 3. Нельзя вызвать явно (вызываетс автоматически при создании класса)
+    static User()
+    {
+        MaxId = 100;
+    }
+
+    // Статический метод
+    // 1. Нет this
+    // 2. Можно использовать только статические поля и св-ва
+    public static void ResetMaxId() => User.maxId = 0;
+
+    // Статическое св-во
+    public static int MaxId
+    {
+        get => User.maxId;
+        set => User.maxId = value;
+    }
+
+    // Статическое автосв-во
+    public static int InstanceCount { get; set; } = 0;
+
+    // Обычный конструктор
+    public User()
+    {
+        this.id = ++User.MaxId;
+        User.InstanceCount++;
+    }
+}
+
+
+
+
+
+#endregion
+
