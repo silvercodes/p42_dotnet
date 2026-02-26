98a6f0dd525805c1ccf30af80525feee298eb36c
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-02-26 22:51:46 +0300

    add: abstract

--- 06_deep_oop/01_inheritance/Program.cs
+++ 06_deep_oop/01_inheritance/Program.cs
@@ -186,57 +186,171 @@
 //a.Show();
 
 
-using System.Runtime.InteropServices.Marshalling;
-
-User b = new Student(101, 23);
-b.Age = 34;
-b.Show();
-b.Age = 15;
-b.Show();
+//User b = new Student(101, 23);
+//b.Age = 34;
+//b.Show();
+//b.Age = 15;
+//b.Show();
 
 
 
-class User
-{
-    public int Id { get; set; }
-    public virtual int Age { get; set; }
-    public User(int id, int age)
-    {
-        Id = id;
-        Age = age;
-    }
+//class User
+//{
+//    public int Id { get; set; }
+//    public virtual int Age { get; set; }
+//    public User(int id, int age)
+//    {
+//        Id = id;
+//        Age = age;
+//    }
 
-    public virtual void Show()
-    {
-        Console.WriteLine("User::Show()");
-        Console.WriteLine($"id: {Id}, age: {Age}");
-    }
+//    public virtual void Show()
+//    {
+//        Console.WriteLine("User::Show()");
+//        Console.WriteLine($"id: {Id}, age: {Age}");
+//    }
 
-    //public static virtual void Method()       // Не имеет смысла
-    //{ 
-    //}
-}
+//    //public static virtual void Method()       // Не имеет смысла
+//    //{ 
+//    //}
+//}
 
-class Student: User
-{
-    // public override int Age { get; set; }        // :-( (дублируется поле age....)
-    public override int Age
-    {
-        get => base.Age;
-        set
-        {
-            base.Age = value >= 18 ? value : 18;
-        }
-    }
-    public Student(int id, int age) : 
-        base(id, age)
-    {}
-    public override sealed void Show()
-    {
-        Console.WriteLine("Student::Show()");
-        Console.WriteLine($"id: {Id}, age: {Age}");
-    }
-}
+//class Student: User
+//{
+//    // public override int Age { get; set; }        // :-( (дублируется поле age....)
+//    public override int Age
+//    {
+//        get => base.Age;
+//        set
+//        {
+//            base.Age = value >= 18 ? value : 18;
+//        }
+//    }
+//    public Student(int id, int age) : 
+//        base(id, age)
+//    {}
+//    public override sealed void Show()
+//    {
+//        Console.WriteLine("Student::Show()");
+//        Console.WriteLine($"id: {Id}, age: {Age}");
+//    }
+//}
+
+
+
+
+//E e = new E();
+//e.Show();
+
+//D d = new D();
+//d.Show();
+
+//A a = new D();
+//a.Show();
+
+//B b = new E();
+//b.Show();
+
+//object o = new C();
+//// o.Show();               // ERROR
+
+//A a = new C();
+//a.Show();
+
+
+//D d = new E();
+//d.Show();
+
+//class A
+//{
+//    public virtual void Show() => Console.WriteLine("A Show()");
+//}
+//class B: A
+//{
+
+//}
+//class C: B
+//{
+//    // public override void Show() => Console.WriteLine("C Show()");
+//}
+//class D: C
+//{
+
+//}
+//class E: D
+//{
+//    public override void Show() => Console.WriteLine("E Show()");
+//}
 
 #endregion
 
+
+#region Hiding
+
+
+//Admin admin = new Admin(101, "admin@mail.com");
+//admin.Show();
+
+//User user = new Admin(102, "vasia@mail.com");
+//user.Show();
+
+
+//Admin admin = new Admin(101, "admin@mail.com");
+//admin.Email = "chack@mail.com";
+//admin.Show();
+
+
+//User user = new Admin(101, "admin@mail.com");
+//user.Email = "chack@mail.com";
+//user.Show();
+
+//if (user is Admin a)
+//    a.Show();
+
+
+//Console.WriteLine(User.minAge);
+//Console.WriteLine(Admin.minAge);
+
+//Console.WriteLine(User.Pi);
+//Console.WriteLine(Admin.Pi);
+
+class User
+{
+    public readonly static int minAge = 18;
+    public const double Pi = 3.14;
+    public int Id { get; set; }
+    public string Email { get; set; } = "no_email";
+    public User(int id, string email)
+    {
+        Id = id;
+        Email = email;
+    }
+    public void Show()
+    {
+        Console.WriteLine($"User: {Email}");
+    }
+}
+
+class Admin: User
+{
+    public new readonly static int minAge = 30;
+    // public new const double Pi = 3.1415;
+    // public new string Email { get; set; }           // FU FU FU :-(
+    public new string Email
+    {
+        get => $"admin: {base.Email}";
+        set => base.Email = value;
+    }
+
+    public Admin(int id, string email):
+        base(id, email)
+    {}
+    public new void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Admin: {Email}");
+    }
+
+}
+
+#endregion
---
+++ 06_deep_oop/02_abstract/Program.cs
@@ -0,0 +1,84 @@
+﻿//      C++                                 C#
+// чистая виртуальная ф-ция         абстракный метод (является виртуальным)
+// абстрактный класс                абстрактный класс
+// интерфейсный класс               интерфейс
+
+// Абстрктные члены классы
+// 1. Методы
+// 2. Свойства
+// 3. Соытия
+// 4. Индексаторы
+
+
+//abstract class Unit
+//{
+//    public int Hp { get; protected set; }       // АВТОСВОЙСТВО
+//    public abstract int Force { get; set; }     // НЕ АВТОСВОЙСТВО (поле не создаётся, разворачивается в 2 абстрактных метода)
+//    public abstract int Points { get; set; }    // НЕ АВТОСВОЙСТВО (поле не создаётся, разворачивается в 2 абстрактных метода)
+//    public abstract void Defence();
+//    public virtual void Move()
+//    {
+//        Console.WriteLine("Unit Move()");
+//    }
+//    public void Attack()
+//    {
+//        Console.WriteLine("Unit Attack()");
+//    }
+//}
+
+//class Warrior: Unit
+//{
+//    public override int Force { get; set; }     // АВТОСВОЙСТВО
+
+//    private int points;
+//    public override int Points
+//    {
+//        get => points;
+//        set => points = value;
+//    }
+//    public override void Defence()
+//    {
+//        Console.WriteLine("Warrior Defence()");
+//    }
+//    public override void Move()
+//    {
+//        Console.WriteLine("Warrior Move()");
+//    }
+//}
+
+//class SuperWarrior: Warrior
+//{
+//    public override void Defence()
+//    {
+//        Console.WriteLine("SuperWarrior Defence()");
+//    }
+//}
+
+
+//abstract class Archer: Unit
+//{
+
+//}
+
+
+
+// =========== Object class virtual methods
+
+object p = new Product()
+{
+    Id = 101,
+    Title = "Apple"
+};
+
+Console.WriteLine(p);
+
+class Product
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "no_title";
+
+    public override string ToString()
+    {
+        return $"id: {Id}, title: {Title}";
+    }
+}
