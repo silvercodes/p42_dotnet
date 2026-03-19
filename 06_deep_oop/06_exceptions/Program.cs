b630fb7a769d24f184f359959c9c8734c2500be7
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-03-19 21:27:14 +0300

    add:delegates

--- 06_deep_oop/06_exceptions/Program.cs
+++ 06_deep_oop/06_exceptions/Program.cs
@@ -68,29 +68,105 @@
 
 
 
-void F4()
+//void F4()
+//{
+//    int a = 0;
+//    int result = 4 / a;
+//    Console.WriteLine("End of F4()");
+//}
+
+//void F3()
+//{
+//    F4();
+//    Console.WriteLine("End of F3()");
+//}
+//void F2()
+//{
+//    F3();
+//    Console.WriteLine("End of F2()");
+//}
+
+//void F1()
+//{
+//    F2();
+//    Console.WriteLine("End of F1()");
+//}
+
+//F1();
+
+
+
+
+
+// =========================
+
+
+
+using System.Data;
+
+Account acc = new Account(1000);
+
+try
 {
-    int a = 0;
-    int result = 4 / a;
-    Console.WriteLine("End of F4()");
+    acc.Withdraw(2000);
+}
+catch (AccountException ex)
+{
+    Console.WriteLine($"System error: {ex.Message}, your sum is {ex.Sum}");
+}
+catch (AppException ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Internal server error");
+}
+finally
+{
+    Console.WriteLine("finally");
 }
 
-void F3()
+
+
+class AppException : Exception
 {
-    F4();
-    Console.WriteLine("End of F3()");
+    public AppException(string? message) : base(message)
+    { }
 }
-void F2()
+class AccountException : AppException
 {
-    F3();
-    Console.WriteLine("End of F2()");
+    public int Sum { get; set; }
+    public AccountException(string? message, int sum) 
+        : base(message)
+    {
+        Sum = sum;
+    }
 }
 
-void F1()
+
+
+class Account
 {
-    F2();
-    Console.WriteLine("End of F1()");
+    public int Balance { get; set; }
+    public Account(int sum) => Balance = sum;
+    public void Withdraw(int sum)
+    {
+        try
+        {
+            if (sum > Balance)
+                throw new AccountException("Sum is unvaliable", sum);
+
+            Balance -= sum;
+
+            Console.WriteLine("All OK");
+        }
+        catch (AccountException ex)
+        {
+            Console.WriteLine("Account exception LOG handler");
+
+            // throw new AppException("APP ERROR");
+            throw;
+        }
+    }
 }
-
-F1();
-
---
+++ 06_deep_oop/07_delegates/Program.cs
@@ -0,0 +1,244 @@
+﻿
+
+
+#region Intro
+//ExecuteDelegate del = new ExecuteDelegate(f1);
+//del += f1;
+//del += f1;
+//del += f1;
+
+//// del += f2;      // ERROR
+
+//del();
+//del?.Invoke();
+
+
+//ExecuteDelegate del1 = new ExecuteDelegate(f1);
+//ExecuteDelegate del2 = new ExecuteDelegate(f1);
+
+//ExecuteDelegate del3 = del1 + del2;
+//del3.Invoke();
+
+
+//void f1()
+//{
+//    Console.WriteLine("f1");
+//}
+
+//void f2(int x = 0)
+//{
+//    Console.WriteLine($"x = {x}");
+//}
+
+
+//delegate void ExecuteDelegate(); 
+#endregion
+
+#region Example_1
+
+//User u = new User();
+//u.Process("Hello user");
+
+//class User
+//{
+//    private delegate void NotifyHandler(string message);
+
+//    public void Process(string message)
+//    {
+//        NotifyHandler handler;
+//        handler = Notify;
+//        handler += TelegramNotify;
+
+//        //
+//        //
+
+//        handler?.Invoke(message);
+//    }
+
+//    private void Notify(string message)
+//    {
+//        Console.WriteLine($"Notify: {message}");
+//    }
+//    private void TelegramNotify(string message)
+//    {
+//        Console.ForegroundColor = ConsoleColor.Green;
+//        Console.WriteLine($"Telegram: {message}");
+//        Console.ResetColor();
+//    }
+//}
+
+
+
+
+
+//NotifyHandler? notifier = SendEmail;
+//notifier += SendSms;
+//notifier += SendTelegram;
+
+//notifier -= SendSms;
+
+//notifier?.Invoke("Content");
+
+
+//void SendEmail(string content) => Console.WriteLine($"Email: {content}");
+//void SendTelegram(string content) => Console.WriteLine($"Telegram: {content}");
+//void SendSms(string content) => Console.WriteLine($"Sms: {content}");
+
+
+//delegate void NotifyHandler(string content);
+
+#endregion
+
+#region Generic delegates
+
+
+//Operation<string, int> op = Process;
+//Console.WriteLine(op(5, 6));
+
+//string Process(int x, int y) => $"{x} + {y} = {x + y}";
+
+//delegate T Operation<T, K>(K a, K b);
+
+// ----------------------------------------------------------------
+
+//int? Execute(int a, int b, Operation<int> op)
+//{
+//    return op?.Invoke(a, b);
+//}
+
+//// Console.WriteLine(Execute(3, 4, Add));
+//Console.WriteLine(Execute(3, 4, new Operation<int>(Mul)));
+
+
+
+//Operation<int> GetOperation(OpType opType) => opType switch
+//{
+//    OpType.Add => Add,
+//    OpType.Mult => Mul,
+//    OpType.Sub => Sub,
+//    OpType.Division => Div,
+//    _ => throw new NotSupportedException()
+//};
+
+//Operation<int> operation = GetOperation(OpType.Mult);
+//Console.WriteLine(operation?.Invoke(3, 4));
+
+
+//int Add(int a, int b) => a + b;
+//int Sub(int a, int b) => a - b;
+//int Mul(int a, int b) => a * b;
+//int Div(int a, int b) => a / b;
+
+//enum OpType
+//{
+//    Add,
+//    Mult,
+//    Sub,
+//    Division,
+//}
+
+
+//delegate T Operation<T>(T a, T b);
+
+
+#endregion
+
+
+
+#region Practice Observer pattern
+
+// === Классическая реализация
+
+Account acc = new Account(1000);
+acc.AddSubscriber(new Logger("app.log"));
+acc.AddSubscriber(new TelegramNotifier());
+acc.AddSubscriber(new EmailNotifier());
+
+acc.Add(100);
+acc.Withdraw(500);
+acc.Withdraw(1000);
+
+
+interface ISubscriber
+{
+    void Update(string message);
+}
+
+class TelegramNotifier : ISubscriber
+{
+    public void Update(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Telegram: {message}");
+        Console.ResetColor();
+    }
+}
+class EmailNotifier : ISubscriber
+{
+    public void Update(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"Email: {message}");
+        Console.ResetColor();
+    }
+}
+class SmsNotifier : ISubscriber
+{
+    public void Update(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"SMS: {message}");
+        Console.ResetColor();
+    }
+}
+class Logger : ISubscriber
+{
+    public string Path { get; set; }
+    public Logger(string path) => Path = path;
+    public void Update(string message)
+    {
+        Console.WriteLine($"Logging to {Path}: {message}");
+    }
+}
+
+class Account
+{
+    private int sum;
+    private List<ISubscriber> subscribers = new List<ISubscriber>();
+    public Account(int sum) => this.sum = sum;
+    public void AddSubscriber(ISubscriber subscriber) => subscribers.Add(subscriber);
+
+    public void Add(int val)
+    {
+        sum += val;
+
+        Emit($"ADDED: {val}");
+    }
+
+    public void Withdraw(int val)
+    {
+        if (sum >= val)
+        {
+            sum -= val;
+
+            Emit($"WITHDRAWEN: {val}");
+        }
+        else
+        {
+            Emit($"ERROR: {val}");
+        }
+    }
+
+    private void Emit(string message)
+    {
+        foreach (ISubscriber subscriber in subscribers)
+        {
+            subscriber.Update(message);
+        }
+    }
+}
+
+
+#endregion
+
+
